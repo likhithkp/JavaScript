5d4ce66a82d6daa2eac6af33697e3f7681e57e45
Likhith K.P. <likhithkpdev@gmail.com>
2024-05-06 02:21:38 +0000

    Practiced array methods slice and splice

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-forEach(() => {
-  console.log(":HEllo");
-});
+const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("slice", a.slice(1, 4));
+
+const x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("splice", x.splice(2, 5));
